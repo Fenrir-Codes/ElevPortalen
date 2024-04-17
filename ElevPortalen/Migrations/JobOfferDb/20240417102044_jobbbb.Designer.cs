3b72ae4d39042b60e1ce321d0bb29e044d174f5e
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2024-04-17 13:16:27 +0200

    jobofferchanged

--- ElevPortalen/Migrations/JobOfferDb/20240417102044_jobbbb.Designer.cs
+++ ElevPortalen/Migrations/JobOfferDb/20240417102044_jobbbb.Designer.cs
@@ -4,6 +4,7 @@ using ElevPortalen.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElevPortalen.Migrations.JobOfferDb
 {
     [DbContext(typeof(JobOfferDbContext))]
-    partial class JobOfferDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240417102044_jobbbb")]
+    partial class jobbbb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ElevPortalen/Migrations/JobOfferDb/20240417102044_jobbbb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElevPortalen.Migrations.JobOfferDb
+{
+    /// <inheritdoc />
+    public partial class jobbbb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPerson",
+                table: "JobOfferDataBase",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PhoneNumber",
+                table: "JobOfferDataBase",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactPerson",
+                table: "JobOfferDataBase");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "JobOfferDataBase");
+        }
+    }
+}
